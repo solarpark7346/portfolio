02f4d0c551d4a90d2267dc6e08149153ce26b5ba
Park, Boseong <solarpark7346@gmail.com>
2021-12-28 02:02:01 +0900

    Updates

---
+++ precache-manifest.ea76d2e98c7d29b98717ab2c16a915ca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabff7ee8e9057197e56a0678b59d135",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d46c83e4aec95d977279",
+    "url": "/portfolio/static/css/main.296dfb72.chunk.css"
+  },
+  {
+    "revision": "5e94758350f2e8dd3f3e",
+    "url": "/portfolio/static/js/2.251464ae.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/portfolio/static/js/2.251464ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb7c5051d1460b31e73",
+    "url": "/portfolio/static/js/3.80ffe179.chunk.js"
+  },
+  {
+    "revision": "d46c83e4aec95d977279",
+    "url": "/portfolio/static/js/main.6da1f53e.chunk.js"
+  },
+  {
+    "revision": "cd85a2853632727b4bc5",
+    "url": "/portfolio/static/js/runtime-main.d87ddb8b.js"
+  },
+  {
+    "revision": "ad133bf97bc0c5e57c22708496e9dcdc",
+    "url": "/portfolio/static/media/Image loading.ad133bf9.png"
+  },
+  {
+    "revision": "cf95acdbc225016cb640dfb6bc639a3f",
+    "url": "/portfolio/static/media/PBS.cf95acdb.jpg"
+  },
+  {
+    "revision": "6b2af5029c09960384765c114b744aaf",
+    "url": "/portfolio/static/media/aladdin logo design.6b2af502.png"
+  },
+  {
+    "revision": "bd9d85114f011d4c4e756d6a1e73e3ad",
+    "url": "/portfolio/static/media/banner.bd9d8511.gif"
+  },
+  {
+    "revision": "2acaf84379d0edbf5bce8a380192288d",
+    "url": "/portfolio/static/media/xd.2acaf843.png"
+  }
+]);
