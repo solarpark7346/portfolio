b2fc8bf582f2ac1d60b703018b088e62801faecb
solarpark7346 <solarpark7346@gmail.com>
2022-06-28 18:05:44 +0900

    Updates

---
+++ precache-manifest.64a4c8aa9df65fc60ec9145acdd46c25.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ef0898284c52764f7fd13072a17ac6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "70925c33e3534c8f6994",
+    "url": "/portfolio/static/css/main.72310c75.chunk.css"
+  },
+  {
+    "revision": "c916d8496873192fa1a2",
+    "url": "/portfolio/static/js/2.2eb6e722.chunk.js"
+  },
+  {
+    "revision": "106d4aa95ab453b26b33ddade6518aa3",
+    "url": "/portfolio/static/js/2.2eb6e722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f42be3463da36c6b106",
+    "url": "/portfolio/static/js/3.9fc2c06b.chunk.js"
+  },
+  {
+    "revision": "70925c33e3534c8f6994",
+    "url": "/portfolio/static/js/main.38aa5ddd.chunk.js"
+  },
+  {
+    "revision": "1f5d9de977d3f2586c64",
+    "url": "/portfolio/static/js/runtime-main.ee6ff9f2.js"
+  },
+  {
+    "revision": "84cd2c3f2cd25d958fde6d8d7aed89bc",
+    "url": "/portfolio/static/media/GmarketSansLight.84cd2c3f.otf"
+  },
+  {
+    "revision": "dcdb13e415566997f4a392e29ddfdd90",
+    "url": "/portfolio/static/media/GmarketSansMedium.dcdb13e4.otf"
+  },
+  {
+    "revision": "ad133bf97bc0c5e57c22708496e9dcdc",
+    "url": "/portfolio/static/media/Image loading.ad133bf9.png"
+  },
+  {
+    "revision": "4472c13edef677f6e191cc8161214e6d",
+    "url": "/portfolio/static/media/PBS_imgg.4472c13e.jpeg"
+  },
+  {
+    "revision": "e6d5be6b0447f46aee48fce742f501a2",
+    "url": "/portfolio/static/media/ahahclip_logo.e6d5be6b.jpg"
+  },
+  {
+    "revision": "90167d0da95679d82d3f67b1cecb9ce1",
+    "url": "/portfolio/static/media/archive_logo.90167d0d.png"
+  },
+  {
+    "revision": "eb56e9ab639a9c708e51f2ef4a6bde61",
+    "url": "/portfolio/static/media/ninefruit_logo.eb56e9ab.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.06eb3522e17f5756d76739f574e864db.js"
+  "/portfolio/precache-manifest.64a4c8aa9df65fc60ec9145acdd46c25.js"
 );
 
 self.addEventListener('message', (event) => {
